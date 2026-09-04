04d9b8fe524c1bec9017d02e6760da88ed731c7e
Aryan Choudhary <aryan.choudhary020805@gmail.com>
2026-09-04 21:56:21 +0530

    Add: string-encode-and-decode - submission-2

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-2.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string encode(vector<string>& strs) {
+        string enc = "";
+        for (auto it : strs){
+            enc+=to_string(it.size())+"#"+it;
+        }
+        return enc;
+    }
+    vector<string> decode(string s) {
+        vector <string> dec;
+        int i = 0;
+        while(i<s.size()){
+            int j=i;
+            while(s[j] != '#'){
+                j++;
+            }
+            int len = stoi(s.substr(i, j-i));
+            dec.push_back(s.substr(j+1, len));
+            i = j +1+len;   
+        }
+        return dec;
+    }
+};
